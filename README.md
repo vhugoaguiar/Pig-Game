b823467378fec92425420dffde94f9b50244f75e
Victor Hugo Santos Aguiar <vhugoaguiar@gmail.com>
2023-11-20 23:00:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Pig Game
+**Author:** [Victor Hugo Santos Aguiar](https://github.com/vhugoaguiar)
+
+## Overview
+This JavaScript project represents a web-based implementation of the classic dice game "Pig." It showcases my skills in web development, JavaScript, and DOM manipulation. The project is inspired by the "Pig Game" section of Udemy's course, "The Complete JavaScript Course 2024: From Zero to Expert!" by Jonas Schmedtmann, but with a personalized touch in code structure and logic.
+
+## Project Description
+### Objective
+The objective of the "Pig Game" is to accumulate a target score by rolling a six-sided die. Players take turns rolling the die and choosing whether to hold their current score or continue rolling. The first player to reach or exceed the target score wins.
+
+### Gameplay
+- Players roll a six-sided die and accumulate points equal to the rolled number.
+- If a player rolls a 1, their current turn ends, and they lose all points for that turn.
+- Players can choose to "Hold" their current score, adding it to their total score and ending their turn.
+- The first player to reach or exceed the target score wins the game.
+
+### Dynamic UI
+The user interface is dynamically updated using DOM manipulation to reflect the current game state, scores, and active player.
+
+## Notable Features
+- **Enhanced Game Logic:** Personalized game logic for an improved and customized gaming experience.
+- **Code Refactoring:** The code has been refactored to enhance readability, maintainability, and adherence to best practices.
+- **Interactive UI:** Users can easily interact with the game through intuitive buttons and visual feedback.
+
+## Credits
+- Original Game Concept: Udemy's "The Complete JavaScript Course 2024: From Zero to Expert!" by Jonas Schmedtmann
+- **Author:** [Victor Hugo Santos Aguiar](https://github.com/vhugoaguiar)
+
+## Author's Note
+This project is a significant step in my journey towards becoming a skilled web developer. It highlights my proficiency in JavaScript, DOM manipulation, and my commitment to writing clean and efficient code. Feel free to explore the codebase, provide feedback, and enjoy playing the "Pig Game"!
+
+Thank you for checking out my project!
---
+++ script.js
@@ -0,0 +1,110 @@
+/*
+  Dice Game Script
+
+  This script implements a simple two-player dice game.
+*/
+'use strict';
+
+// Selecting visual elements
+const selectElement = (selector) => document.querySelector(selector);
+
+const player = [selectElement('.player--0'), selectElement('.player--1')];
+const totalScoreEl = [selectElement('#score--0'), selectElement('#score--1')];
+const currentEl = [selectElement('#current--0'), selectElement('#current--1')];
+const diceEl = selectElement('.dice');
+
+// Selecting buttons
+const btnNew = selectElement('.btn--new');
+const btnRoll = selectElement('.btn--roll');
+const btnHold = selectElement('.btn--hold');
+
+// Global variables
+let scores;
+let currentScore;
+let activePlayer; // Holds the information of which player is active
+let gameActive; // Game state control (false when the game is over)
+
+// Initialization function
+function init() {
+  // Reset all the variables and visual elements
+  totalScoreEl[0].textContent = 0;
+  totalScoreEl[1].textContent = 0;
+  currentEl[0].textContent = 0;
+  currentEl[1].textContent = 0;
+  // Hide the dice
+  diceEl.classList.add('hidden');
+  // Remove winner class if present
+  player[0].classList.remove('player--winner');
+  player[1].classList.remove('player--winner');
+  // Set player1 (position 0) to active
+  player[1].classList.remove('player--active');
+  player[0].classList.add('player--active');
+  scores = [0, 0];
+  currentScore = 0;
+  // Reset the active player to 0
+  activePlayer = 0;
+  // Set gameActive to true
+  gameActive = true;
+}
+
+// Initializing the game
+init();
+
+// Switch players function
+function switchPlayer() {
+  player[activePlayer].classList.toggle('player--active');
+  currentEl[activePlayer].textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player[activePlayer].classList.toggle('player--active');
+}
+
+// Rolling dice functionality
+// As the function will not be reused, I can write it directly
+btnRoll.addEventListener('click', () => {
+  if (gameActive) {
+    // Only executes if the game is not finished
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // .trunc removes the decimal part, .random gives a number between 0 and 0.999, *6 sets the range to 0 to 5.999 and +1 makes it from 1 to 6;
+    diceEl.classList.remove('hidden');
+    // Remove the hidden class if present
+    diceEl.src = `dice-${dice}.png`;
+    // Set the dice image to match the rolled number
+    // If the rolled number is not a one, update the score, else switch players
+    if (dice !== 1) {
+      // Add the rolled number to the current score
+      currentScore += dice;
+      // Update the current score
+      currentEl[activePlayer].textContent = currentScore;
+    } else {
+      // Switch the active player
+      switchPlayer();
+    }
+  }
+});
+
+// Holding the score functionality
+btnHold.addEventListener('click', () => {
+  if (gameActive) {
+    // Only executes if the game is not finished
+    scores[activePlayer] += currentScore;
+    // Adds the player current store to his total score
+    totalScoreEl[activePlayer].textContent = scores[activePlayer];
+    // Displays the new total score
+    if (scores[activePlayer] >= 20) {
+      // Player wins
+      gameActive = false;
+      // Game ends
+      player[activePlayer].classList.add('player--winner');
+      // Adds the "winner" visuals
+      player[activePlayer].classList.remove('player--active');
+      // Removes active class
+      diceEl.classList.add('hidden');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+// New game functionality (reset the game at any point)
+btnNew.addEventListener('click', init);
